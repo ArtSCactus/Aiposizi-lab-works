489a37fc5479a3f4491925efcc27fb53f99a64ee
ArtSCactus <artemir14@gmail.com>
2020-05-30 13:36:25 +0300

    Update UserRepository.java

--- src/main/java/com/bsuir/user/UserRepository.java
+++ src/main/java/com/bsuir/user/UserRepository.java
@@ -11,9 +11,9 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
     User findByName(String name);
-    @Query("select id, authority, email, google_id, name, password from university.accounts where name=? and encode(digest(?, 'sha256'), 'hex')")
+    @Query("select id, authority, email, google_id, name, password from university.accounts where name=? and encode(digest(:password, 'sha256'), 'hex')")
     User findByNameAndPassword(String name, String password);
     @Modifying
-    @Query("insert into university.accounts (id, authority, email, google_id, name, passowrd) values (?, ?, ?, ?, ?, encode(digest(?, 'sha256'), 'hex'))")
+    @Query("insert into university.accounts (id, authority, email, google_id, name, passowrd) values (:id, :authority, :email, :googleId, :name, encode(digest(:password, 'sha256'), 'hex'))")
     User save(User user);
 }
